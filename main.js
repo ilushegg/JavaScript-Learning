4aba1c819c583aba6517ad6aabf6833a5a93abb7
ilushegg <marnopolskiyi@mail.ru>
2022-08-07 12:38:44 +0300

    Basic JavaScript DONE

--- main.js
+++ main.js
@@ -1,6 +1,181 @@
 
 let userName = 'Ivan'
-
 console.log(userName)
 
+userName = 'Anton'
+console.log(userName)
 
+const yearOfBirth = 1990
+
+function sun() {
+    var name = "Kiyv"
+    let namee = 'Kiyv'
+}
+
+sun()
+
+let age = 30
+console.log(age)
+
+let isMarried = true;
+console.log(isMarried)
+
+let someVar = 'Some Value'
+someVar = 50
+someVar = false
+
+let number = null
+console.log(number)
+
+let unDefined
+console.log(unDefined)
+
+console.log(typeof isMarried)
+
+console.log(Number.MAX_SAFE_INTEGER)
+ 
+
+
+
+console.log(10 > 5)
+console.log(20 > 50) 
+
+console.log('5' == 5)
+console.log('5' === 5)
+
+const time = 20
+
+if(time < 12){
+    console.log('Good morning')
+} else if(time >= 12 && time < 18){
+    console.log('Good afternoon')
+} else {
+    console.log('Good evening')
+}
+
+10 < 12 ? console.log('True') : console.log('False')
+
+let greeting = 'Hi, Ivan'
+let howAreYou = 'How are you?'
+
+
+sayHi()
+
+
+
+function sayHi() {
+    console.log(`Hi, ${userName}! How are you?`)
+}
+
+const sayHiExpression = function () {
+    alert ('Hello')
+}
+
+
+function sayHiToUser(name) {
+    console.log(`Hello ${name}!`)
+}
+
+sayHiToUser('Ilya')
+
+function sum(a, b) {
+    const result = a + b;
+    return result
+}
+
+const res = sum (10, 5)
+console.log(res)
+
+console.log(sum(10, 25))
+
+const result = sum(sum(15, 25), 25)
+console.log(result)
+
+function doSomething(func){
+    let x = 10
+    let y = 15
+    return func(x, y)
+}
+
+console.log(doSomething(sum))
+
+function diff (a, b){
+    return a -b 
+}
+
+
+(function () {
+    console.log('hi!')
+})();
+
+(function (a, b) {
+    console.log(a + b)
+}) (10, 15)
+
+const arrowSayHi = (name) => {
+    console.log(`Hi, ${name}!`)
+}
+
+arrowSayHi('Ivan')
+
+const summ = (a, b) => a + b;
+console.log(sum(10, 10))
+
+const auto = ['Audi', 'Mazda', 'BMW', 'Bentley', 5]
+console.log(auto)
+
+auto.push('Nissan')
+console.log(auto)
+
+auto.splice(2, 1)
+
+console.log(auto)
+
+for (let i = 0; i < 10; i+=2) {
+    console.log(i)
+}
+
+for (let item of auto) {
+    console.log(item)
+}
+
+auto.forEach(function(item, index){
+    console.log(item + ' ' + index)
+})
+
+const person = {
+    userName: 'Ivan',
+    age: 21,
+    isMarried: false,
+    sayHi: function() {
+        console.log(`Hi! My name is ${this.userName}.`)
+    }
+}
+
+console.log(person)
+console.log(person.userName)
+console.log(person['age'])
+
+person.profession = 'Developer'
+
+delete person.age
+
+console.log(person)
+person.sayHi()
+
+for (let key in person){
+    console.log(key + ' : ' + person[key])
+}
+
+class Person {
+    constructor(userName, age, isMarried) {
+        this.userName = userName
+        this.age = age
+        this.isMarried = isMarried
+    }
+    sayHi(name) {
+        console.log(`Hi, ${name}! My name is ${this.userName}`)
+    }
+}
+
+const person1 = new Person('Ilya', 19, false)
